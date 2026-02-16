8c80f5562db4d66fc3c78fafb9aee5f4a8a57765
alexis <alexis.ag85@gmail.com>
2026-02-17 00:42:20 +0300

    Rename package to swift-amplitude-ac

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "analytics-connector-ios",
+    name: "swift-amplitude-ac",
     platforms: [
         .iOS(.v10),
         .macOS(.v10_10),
